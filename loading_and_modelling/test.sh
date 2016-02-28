b603596dfd29f71ffa2847c46917faa9afe7cc88
rtimothyweeks <rtimothyweeks@icloud.com>
2016-02-28 16:30:50 -0600

    Added files via upload

---
+++ loading_and_modelling/test.sh
@@ -0,0 +1,7 @@
+/root/start-hadoop.sh
+su - w205
+hdfs dfs -mkdir /user/w205/hospital_compare
+mkdir hospital_raw
+cd hospital_raw/
+wget https://data.medicare.gov/views/bg9k-emty/files/Dlx5-ywq01dGnGrU09o_Cole23nv5qWeoYaL-OzSLSU?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+
