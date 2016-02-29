06b0206f9bb8eb1f93b5cd2419604f552f2e8e36
rtimothyweeks <rtimothyweeks@icloud.com>
2016-02-28 19:33:51 -0600

    Added files via upload

--- loading_and_modelling/hive_base_ddl.sql
+++ loading_and_modelling/hive_base_ddl.sql
@@ -1,17 +1,17 @@
 DROP TABLE hospitals;
 
 CREATE EXTERNAL TABLE hospitals (
-provider_id, string,
-hospital_name, string,
-address, string,
-city, string,
-state, string,
-zip_code, string,
-county_name, string,
-phone_number, string,
-hospital_type, string,
-hospital_ownership, string,
-emergency_services, string,
+provider_id string,
+hospital_name string,
+address string,
+city string,
+state string,
+zip_code string,
+county_name string,
+phone_number string,
+hospital_type string,
+hospital_ownership string,
+emergency_services string,
 )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
