5e0d473d602b55d62546bf074a602176e9cfb23f
rtimothyweeks <rtimothyweeks@icloud.com>
2016-02-28 20:59:12 -0600

    Update hive_base_ddl.sql

--- loading_and_modelling/hive_base_ddl.sql
+++ loading_and_modelling/hive_base_ddl.sql
@@ -21,3 +21,32 @@ WITH SERDEPROPERTIES (
 )
 STORED AS TEXTFILE
 LOCATION "/user/w205/hospital_compare/hospitals";
+
+DROP TABLE effective_care;
+
+CREATE EXTERNAL TABLE effective_care (
+provider_id string,
+hospital_name string,
+address string,
+city string,
+state string,
+zip_code string,
+county_name string,
+phone_number string,
+condition string,
+measure_id string,
+measure_name string,
+score string,
+sample string,
+footnote string,
+measure_start_date date,
+measure_end_date date
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION "/user/w205/hospital_compare/effective_care_ucb.csv/";
